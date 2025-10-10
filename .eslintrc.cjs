80770d11e614e2eef0dcf8846a49d1e21fff1705
Vít Kilián <kilian@marbes.cz>
2025-10-10 11:34:11 +0200

    feat: add tsconfig for testing in theme-management package
    chore: update pnpm-lock.yaml to include husky and lint-staged dependencies
    - Added husky@9.1.7 and lint-staged@16.2.3
    - Updated dependencies for various packages to include yaml@2.8.1
    - Adjusted versioning for several packages to ensure compatibility

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
   extends: ['plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: ['./tsconfig.json', './packages/*/tsconfig.json'],
+    project: ['./tsconfig.json', './packages/*/tsconfig.json', './packages/*/tsconfig.test.json'],
     tsconfigRootDir: __dirname,
   },
   plugins: ['@typescript-eslint'],
